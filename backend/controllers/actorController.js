e69b364d38f0938a2cd8d2ac8cdc6f4059676d35
Saif0319 <gamersaif2@gmail.com>
2023-02-27 18:33:29 -0500

    Created controller

--- backend/controllers/actorController.js
+++ backend/controllers/actorController.js
@@ -1,28 +1,102 @@
+const mongoose = require("mongoose");
+const Actor = require("../models/actorModel")
+
 
 
 
 //GET All Actors
 const getActors = async (req, res) => {
-    await res.status(200).json("ALL ACTORS ARE FETCHED");
+    const actors = await Actor.find({}).sort({firstName: 1});
+
+    res.status(200).json(actors);
 }
 
 
+
+
 //GET one actor
+const getActor = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "Actor does not exist"});
+    }
+
+    const actor = await Actor.findById(id);
+
+    if(!actor) {
+        return res.status(404).json({error: "Actor does not exist"});
+    }
+
+    res.status(200).json(actor);
+}
 
 
 
 //CREATE an actor
 const createActor = async (req, res) => {
-    await res.status(200).json(req.body);
+    const {firstName, lastName, birthday} = req.body;
+
+    try {
+        const actor = await Actor.create({firstName, lastName, birthday});
+        await res.status(200).json(actor);
+    }
+
+    catch(err) {
+        res.status(400).json({error: err.message});
+    }
+
 }
 
+
+
+
 //DELETE an actor
+const deleteActor = async (req, res) => {
+    const { id } = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "Actor does not exist"});
+    }
+
+    const actor = await Actor.findByIdAndDelete({_id: id});
+
+    if(!actor) {
+        res.status(404).json({error: "Actor does not exist"});
+    }
+
+    res.status(200).json(actor);
+}
+
 
 
 
 //UPDATE an actor
+const updateActor = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: "Actor does not exist"});
+    }
+
+    let actor = await Actor.findOneAndUpdate({_id: id}, req.body);
+
+    if(!actor) {
+        res.status(404).json({error: "Actor does not exist"});
+    }
+
+    actor = await Actor.findById(id);
+
+    res.status(200).json(actor);
+
+} 
+
+
 
 module.exports = {
     getActors,
-    createActor
+    getActor,
+    createActor,
+    deleteActor,
+    updateActor
 }
--- backend/routes/actors.js
+++ backend/routes/actors.js
@@ -3,12 +3,20 @@ const actorController = require("../controllers/actorController")
 
 
 
-
+//GET all actors
 router.get("/", actorController.getActors);
 
+//GET one actor
+router.get("/:id", actorController.getActor);
+
+//CREATE an actor
 router.post("/", actorController.createActor);
 
+//DELETE an actor
+router.delete("/:id", actorController.deleteActor);
 
+//UPDATE an actor
+router.patch("/:id", actorController.updateActor);
 
 
 
