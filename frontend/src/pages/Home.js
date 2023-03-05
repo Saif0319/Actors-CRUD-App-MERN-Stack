a35ab14039fbf0dfc7dce89682747ae3f5c51baf
Saif0319 <gamersaif2@gmail.com>
2023-03-04 22:09:05 -0500

    added feature to update actors

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
 
     const [actors, setActors] = useState([]);
     const { dispatch } = useContext(ActorsContext);
+    const [sort, setSort] = useState("fName");
 
     useEffect(() => {
 
@@ -33,19 +34,60 @@ const Home = () => {
         }
 
         fetchActors();
-        
-        
 
-    }, [actors, dispatch])
+    }, [dispatch])
 
 
   return (
     <div>
         <h1 className="text-5xl font-bold text-center mt-10 mb-20">Actors</h1>
-        <div className="grid grid-cols-3 gap-4 w-3/4 h-full">
-            {actors.map(actor => {
-                return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
-            })}
+        <label className="mx-4">Sort by</label>
+        <select className="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2 mx-4 mt-1 mb-10 dark:border-black placeholder-black dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(e) => setSort(e.target.value)}>
+            <option value={"fNameAsc"}>First Name (default)</option>
+            <option value={"fNameDesc"}>First Name (DESC) </option>
+            <option value={"lNameAsc"}>Last Name (ASC)</option>
+            <option value={"lNameDesc"}>Last Name (DESC)</option>
+            <option value={"ageDesc"}>Age (DESC)</option>
+            <option value={"ageAsc"}>Age (ASC)</option>
+        </select>
+        
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 w-3/4 h-full">
+
+            {
+                //Age (Desc)
+                sort === "ageDesc" ? actors.sort((actor1, actor2) => (actor1.birthday < actor2.birthday) ? 1 : -1).map(actor => {
+                    return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
+                })
+
+                // Age (Asc) 
+                : sort === "ageAsc" ? actors.sort((actor1, actor2) => (actor1.birthday < actor2.birthday) ? -1 : 1).map(actor => {
+                    return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
+                })
+
+                // sort by last name Asc
+                : sort === "lNameAsc" ? actors.sort((actor1, actor2) => (actor1.lastName < actor2.lastName) ? -1 : 1).map(actor => {
+                    return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
+                })
+
+                // sort by last name Desc
+                : sort === "lNameDesc" ? actors.sort((actor1, actor2) => (actor1.lastName < actor2.lastName) ? 1 : -1).map(actor => {
+                    return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
+                })
+
+                // sort by first name Desc
+                : sort === "fNameDesc" ? actors.sort((actor1, actor2) => (actor1.firstName < actor2.firstName) ? 1 : -1).map(actor => {
+                    return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
+                })
+                
+
+                // sort by first name Asc (default)
+                : actors.sort((actor1, actor2) => (actor1.firstName < actor2.firstName) ? -1 : 1).map(actor => {
+                    return <Actors fName={actor.firstName} lName={actor.lastName} birthday={actor.birthday} key={actor._id} id={actor._id} />
+                })
+            }
+
+            
         </div>
     </div>
   )
--- frontend/src/pages/Update.js
+++ frontend/src/pages/Update.js
@@ -1,8 +1,86 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 
 const Update = () => {
+
+
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+
+  //Formatting the birthday to be compatible with input type date
+  const bDay = new Date(state.bDay);
+  const month = `${bDay.getMonth() < 10? `0${bDay.getMonth()}`: bDay.getMonth()}`;
+  const day = `${bDay.getDate() < 10? `0${bDay.getDate()}`: bDay.getDate()}`;
+  const formattedBday = [bDay.getFullYear(), month, day].join("-");
+
+
+  const [fName, setFname] = useState(state.fName);
+  const [lName, setLname] = useState(state.lName);
+  const [birthday, setBirthday] = useState(formattedBday);
+  const [error, setError] = useState(null);
+
+
+  const handleBtn = async (e) => {
+    e.preventDefault();
+
+    const actor = {
+        firstName: fName,
+        lastName: lName,
+        birthday: birthday
+    };
+
+
+    const response = await fetch("http://localhost:4000/api/actors/" + state.id, {
+        method: 'PATCH',
+        body: JSON.stringify(actor),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const json = await response.json();
+
+    if (!response.ok) {
+        setError(json.error)
+    }
+
+    //Redirect
+    if(response.ok) {
+        setError(null);
+        return navigate("/");
+    }
+}
+
+
+
   return (
-    <div>Update</div>
+    <div>
+        <form className="border-2 flex flex-col w-1/3 m-4 p-4">
+            <div className="w-3/4">
+                <label htmlFor='fName' className="mx-4">First Name:</label>
+                <input type={"text"} name="fName" value={fName} onChange={(e) => setFname(e.target.value)} className="border-2 border-blue-200 transition-all outline-none m-4 focus:border-blue-500 p-1 w-full" placeholder="First Name" />
+            </div>
+            
+            <div className="w-3/4">
+                <label htmlFor='lName' className="mx-4">Last Name</label>
+                <input type={"text"} name="lName" value={lName} onChange={(e) => setLname(e.target.value)} className="border-2 border-blue-200 transition-all outline-none m-4 focus:border-blue-500 p-1 w-full" placeholder="Last Name" />
+            </div>
+
+            <div className="w-3/4">
+                <label htmlFor='bday' className="mx-4">Birthday</label>
+                <input type={"date"} name="bday" value={birthday} onChange={(e) => setBirthday(e.target.value)} className="border-2 border-blue-200 transition-all outline-none m-4 p-1 focus:border-blue-500 px-1 w-full" />
+            </div>
+
+            <div>
+                <button type='submit' onClick={handleBtn} className="m-4 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full">Update</button>
+            </div>
+
+            {error && <div className="text-xl text-red-600">{error}</div>}
+        </form>
+    </div>
   )
 }
 
